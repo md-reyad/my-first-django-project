3417cea208d54702d578373cd087931be6df0d09
md reyad <imam.reyad93@gmail.com>
2017-12-27 00:37:07 +0600

    crud complete

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('form', views.index, name='index'),
     path('reyad', views.reyad, name='reyad'),
     path('list', views.list, name='list'),
+    path('edit/<id>', views.edit),
+    path('update/<id>', views.update),
+
     # path('saveItem', views.saveItem, name='saveItem'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Book
+from django.shortcuts import redirect
+from django.contrib import messages
+
 
 # Create your views here.
 def hi(request):
@@ -20,7 +23,8 @@ def index(request):
             'description': description,
         }
         # print("::::::::---- this wase title '%s' des is %s " % (name,description) )
-        return render(request, "html/index.html",context= context)
+        messages.add_message(request, messages.SUCCESS, 'Success Your data insert successfully!!')
+        return redirect('list')
 
 
 def about(r):
@@ -32,11 +36,28 @@ def reyad(r):
 
 
 def list(request):
-    book = Book.objects.all()
+    book = Book.objects.raw('select * from polls_book ORDER  by id DESC ')
     context={
         'book':book
     }
     return render(request,"html/list.html",context= context)
+
+
+def edit(request, id):
+    book = Book.objects.get(id=id)
+    context={
+        'book':book
+    }
+    return render(request,"html/edit.html",context= context)
+
+
+def update(request, id):
+    book = Book.objects.get(id=id)
+    book.title=request.POST['title']
+    book.description=request.POST['description']
+    book.save()
+    messages.add_message(request, messages.SUCCESS,"Data Update successfully")
+    return redirect('list')
 # def saveItem(request):
 #     name = status = request.GET('status')
 #     return HttpResponse("save done")
